cf8dde791fdf7017c053565fb0fe8aeaf3c7fe49
sasho.krist <akr@monizze.be>
2022-11-23 11:16:38 +0200

    list drag-drop sort

--- resources/views/livewire/products.blade.php
+++ resources/views/livewire/products.blade.php
@@ -20,45 +20,83 @@
                                 <div class="card">
                                     <div class="card-header">{{ __('Products') }}</div>
                                     <div class="card-body">
-                                        <table class="table">
-                                            <thead>
-                                            <tr>
-                                                <th scope="col">Drag</th>
-                                                <th scope="col">#</th>
-                                                <th scope="col">Name</th>
-                                            </tr>
-                                            </thead>
-                                            @foreach($products as $product)
-                                                <tbody wire:sortable="updateTaskOrder">
-                                                <tr wire:sortable.item="{{ $product->id }}" wire:key="product-{{ $product->id }}" wire:sortable.handle>
-                                                    <th scope="row">|||</th>
-                                                    <th>{{ $product->id }}</th>
-                                                    <td>{{ $product->name }}</td>
-                                                </tr>
-                                                </tbody>
-                                            @endforeach
-                                        </table>
+{{--                                        <table class="table">--}}
+{{--                                            <thead>--}}
+{{--                                            <tr>--}}
+{{--                                                <th scope="col">Drag</th>--}}
+{{--                                                <th scope="col">#</th>--}}
+{{--                                                <th scope="col">Name</th>--}}
+{{--                                            </tr>--}}
+{{--                                            </thead>--}}
+{{--                                            @foreach($products as $product)--}}
+{{--                                                <tbody wire:sortable="updateTaskOrder">--}}
+{{--                                                <tr wire:sortable.item="{{ $product->id }}" wire:key="product-{{ $product->id }}" wire:sortable.handle>--}}
+{{--                                                    <th scope="row">|||</th>--}}
+{{--                                                    <th>{{ $product->id }}</th>--}}
+{{--                                                    <td>{{ $product->name }}</td>--}}
+{{--                                                </tr>--}}
+{{--                                                </tbody>--}}
+{{--                                            @endforeach--}}
+{{--                                        </table>--}}
                                     </div>
+                                    <ul drag-root class="list-group">
+                                        @foreach($products as $product)
+                                        <li drag-item draggable="true" class="list-group-item">|||  {{ $product->name }}</li>
+                                        @endforeach
+                                    </ul>
                                 </div>
                             </div>
                         </div>
                     </div>
                     @livewireScripts
                     <script src="https://cdn.jsdelivr.net/gh/livewire/sortable@v0.x.x/dist/livewire-sortable.js"></script>
-                </div>
+                <script>
+                    let root = document.querySelector('[drag-root]')
+
+                    root.querySelectorAll('[drag-item]').forEach(el => {
+                        el.addEventListener('dragstart', e => {
+                            e.target.setAttribute('dragging', true)
+                        })
+
+                        el.addEventListener('drop', e => {
+                            e.target.classList.remove('bg-blue-100')
+                            let draggingEl = root.querySelector('[dragging]')
+                            e.target.before(draggingEl)
+                        })
+
+                        el.addEventListener('dragenter', e => {
+                            e.target.classList.add('bg-yellow-100')
+                            e.preventDefault()
+                        })
+
+                        el.addEventListener('dragover', e => {
+                            e.preventDefault()
+                        })
+
+                        el.addEventListener('dragleave', e => {
+                            el.classList.remove('bg-blue-100')
+                        })
+
+                        el.addEventListener('dragend', e => {
+                            e.target.removeAttribute('dragging')
+                        })
+                    });
+                </script>
+            </div>
             </div>
         </div>
     </div>
 </body>
 </html>
 
-@push('styles')
-    <styles>
+@push('style')
+    <style>
         .draggable-mirror{
         background-color: white;
         width: 950px;
         display: flex;
         justify-content: space-between;
         }
-    </styles>
+    </style>
 @endpush
+
